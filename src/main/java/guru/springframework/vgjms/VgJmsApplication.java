cbb62d56717bfaf85c248c5f15c4eebd88d40523
vitor <vitorgilcsgmail.com>
2022-09-12 11:01:36 +0100

    Setup the project

---
+++ src/main/java/guru/springframework/vgjms/VgJmsApplication.java
@@ -0,0 +1,13 @@
+package guru.springframework.vgjms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VgJmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VgJmsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/guru/springframework/vgjms/VgJmsApplicationTests.java
@@ -0,0 +1,13 @@
+package guru.springframework.vgjms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VgJmsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
